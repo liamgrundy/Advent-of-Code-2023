015834387c53759ef2e661e7ff6b31a8e8cc7e86
liamgrundy <145603953+liamgrundy@users.noreply.github.com>
2023-12-07 22:45:38 -0500

    Update Main.kt for part 2
    Changes Int to Long and parsed number by removing spaces

--- Day-6/src/main/kotlin/Main.kt
+++ Day-6/src/main/kotlin/Main.kt
@@ -1,19 +1,18 @@
 import java.io.File
 
-fun calculateDistance(holdButtonTime: Int, raceTime: Int) : Int {
+fun calculateDistance(holdButtonTime: Long, raceTime: Long) : Long {
     return holdButtonTime * (raceTime - holdButtonTime)
 }
 
 fun main() {
     val file = File("res/data.txt").readLines()
 
-    val time = Regex("\\b\\d+\\b").findAll(file[0]).map { it.value.toInt() }.toList()
-    val distance = Regex("\\b\\d+\\b").findAll(file[1]).map { it.value.toInt() }.toList()
+    val time = Regex("\\b\\d+\\b").findAll(file[0].replace(" ", "")).map { it.value.toLong() }.toList()
+    val distance = Regex("\\b\\d+\\b").findAll(file[1].replace(" ", "")).map { it.value.toLong() }.toList()
 
     val numberOfWays = time.zip(distance).map { (t, d) ->
-        List(t.toInt()) { it }.count { calculateDistance(it.toInt(), t) > d }
+        List(t.toInt()) { it }.count { calculateDistance(it.toLong(), t) > d }
     }
 
-    println("Number of ways to win each race: $numberOfWays")
-    println("Product: ${numberOfWays.fold(1) { t, it -> t * it }}")
+    println("Number of ways to win race: ${numberOfWays.first()}")
 }
